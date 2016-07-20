c04ad0e1bf3b89f6761c803a584dc182972a1287
Isaac Yoon <realisy@gmail.com>
2016-07-20 18:55:28 -0400

    recursion done

--- pop.rb
+++ pop.rb
@@ -2,13 +2,29 @@ POP_CONSTANT = 2
 
 def calc(money)
 	res = money / POP_CONSTANT
-	bnum = redeem_bottle(res)
-	cnum = redeem_cap(res)
-	redemp_bottle = bnum + cnum
-	lobnum = leftover_bottle(res)
-	loccum = leftover_cap(res)
-	puts "For the #{money} dollars you have spent, you have purchased #{res} pop. You may redeem #{bnum}pop with empty bottles, #{cnum}pop with empty caps in total of #{redemp_bottle}"
-	puts "You also have #{lobnum}bottles and #{loccum}caps left for future redemption"
+	@total_bottles = res
+	calc_recycled(res, res)
+	# bnum = redeem_bottle(res)
+	# cnum = redeem_cap(res)
+	# redemp_bottle = bnum + cnum
+	# lobnum = leftover_bottle(res)
+	# loccum = leftover_cap(res)
+	# puts "For the #{money} dollars you have spent, you have purchased #{res} pop. You may redeem #{bnum}pop with empty bottles, #{cnum}pop with empty caps in total of #{redemp_bottle}"
+	# puts "You also have #{lobnum}bottles and #{loccum}caps left for future redemption"
+	@total_bottles
+end
+
+def calc_recycled(bottles, caps)
+	new_bottles = redeem_bottle(bottles)
+	new_caps = redeem_cap(caps)
+	remaining_bottles = leftover_bottle(bottles)
+	remaining_caps = leftover_cap(caps)
+	new_total_bottles = new_bottles + remaining_bottles + new_caps
+	new_total_caps = new_bottles + new_caps + remaining_caps
+	@total_bottles += (new_bottles + new_caps)
+	unless new_total_bottles < 2 && new_total_caps < 4
+		calc_recycled(new_total_bottles, new_total_caps)
+	end
 end
 
 def redeem_bottle(res)
@@ -35,7 +51,7 @@ loop do
 	case money_spent
 		when /[0-9]+/
 			money = money_spent.to_i
-			calc(money)
+			puts calc(money)
 		when 'quit'
 			break
 		else
--- recursion_test.rb
+++ recursion_test.rb
@@ -1,44 +1,83 @@
-
+require 'pry'
 # keep on looping redeem_bottle and redeem_cap until leftover_bottle < 2 && leftover_cap < 4
 POP_CONSTANT = 2
 
-def calc(money)
-	res = money / POP_CONSTANT
-	bnum = redeem_bottle(res)
-	cnum = redeem_cap(res)
-	redemp_bottle = bnum + cnum
-	lobnum = leftover_bottle(res)
-	locnum = leftover_cap(res)
-	# puts "For the #{money} dollars you have spent, you have purchased #{res} pop. You may redeem #{bnum}pop with empty bottles, #{cnum}pop with empty caps in total of #{redemp_bottle}"
-	# puts "You also have #{lobnum}bottles and #{loccum}caps left for future redemption"
-	(1..res).each do |i|
-		redeem_bottle(i)
-		redeem_cap(i)
+
+def calc_recycled(bottles, caps)
+	# binding.pry
+	recycled = 0
+	loop do
+		bottles -= 2
+		recycled += 1
+		caps -= 4
+		recycled += 1
+		if bottles < 2 && caps < 4
+			break
+		end
 	end
-end
-
-def redeem_bottle(res)
-	res / 2
-end
-
-def redeem_cap(res)
-	res / 4
-end
-
-def leftover_bottle(res)
-	res % 2
-end
-
-def leftover_cap(res)
-	res % 4
+	recycled
 end
 
 
-puts calc(20)
-# totalbottles = 0
+
+puts calc_recycled(1, 1)
+
+# def calc(money)
+# 	init = calc_init_bottle(money)
+# 	recy = redeem_cap(init) + redeem_bottle(init)
+# 	init += recy
+# 	init
+# 	# loop do
+# 	# 	if resb >= 0
+# 	# 		res = resb
+# 	# 		res +=  resb
+# 	# 		break
+# 	# 	end
+# 	# end
+# 	# res
+# 	# binding.pry
+# end
 #
-# totalbottles = res + redemp_bottle
-# redemp_bottle = bnum + cnum
+# 	# bnum = redeem_bottle(res)
+# 	# cnum = redeem_cap(res)
+# 	# redemp_bottle = bnum + cnum
+# 	# lobnum = leftover_bottle(res)
+# 	# locnum = leftover_cap(res)
+#
+# 	# puts "For the #{money} dollars you have spent, you have purchased #{res} pop. You may redeem #{bnum}pop with empty bottles, #{cnum}pop with empty caps in total of #{redemp_bottle}"
+# 	# puts "You also have #{lobnum}bottles and #{locnum}caps left for future redemption"
+# def calc_init_bottle(money)
+# 	money / POP_CONSTANT
+# end
+#
+# def redeem_bottle(res)
+# 	res / 2
+# end
+#
+# def redeem_cap(res)
+# 	res / 4
+# end
+#
+# def leftover_bottle(res)
+# 	res % 2
+# end
+#
+# def leftover_cap(res)
+# 	res % 4
+# end
+#
+# def recursion(res)
+# end
+#
+# puts calc(23)
 
 
-
+# puts test(20)
+#
+# 	unless lobnum < 2 && locnum < 4 && redemp_bottle == 0
+# 		redeem_bottle(res)
+# 		redeem_cap(res)
+# 	else
+# 		return res
+# 	end
+# 	calc()
